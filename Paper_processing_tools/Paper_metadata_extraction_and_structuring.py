1265a9e670e95c633e6b36655f98331a51ce7d9f
King guys <68528929+2404589803@users.noreply.github.com>
2024-03-13 14:15:08 +0800

    Delete collect_papers workflow

---
+++ Paper_processing_tools/Paper_metadata_extraction_and_structuring.py
@@ -0,0 +1,19 @@
+import json
+
+# 读取JSON文件
+with open("D:\\python project\\hf-daily-paper-newsletter-chinese\\daily_papers\\daily_papers_2024-03-12.json", "r") as file:
+    data = json.load(file)
+
+# 提取论文信息并按指定顺序排列
+papers_clean = [{"title": paper["paper"]["title"],
+                "id": paper["paper"]["id"],
+                "summary": paper["paper"]["summary"]} for paper in data]
+
+# 生成新文件名
+original_filename = "D:\\python project\\hf-daily-paper-newsletter-chinese\\daily_papers_clean\\daily_papers_2024-03-12.json"
+filename_parts = original_filename.split(".")
+new_filename = f"{filename_parts[0]}_clean.{filename_parts[1]}"
+
+# 保存到新文件
+with open(new_filename, "w") as file:
+    json.dump(papers_clean, file, indent=4)
