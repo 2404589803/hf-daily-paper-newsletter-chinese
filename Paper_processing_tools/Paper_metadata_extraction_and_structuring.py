52badfd631aeaca7f5007876d264fa87b0287436
King guys <68528929+2404589803@users.noreply.github.com>
2024-03-26 00:17:57 +0800

    Update file paths and filenames for daily papers

--- Paper_processing_tools/Paper_metadata_extraction_and_structuring.py
+++ Paper_processing_tools/Paper_metadata_extraction_and_structuring.py
@@ -1,7 +1,7 @@
 import json
 
 # 读取JSON文件
-with open("D:\\python project\\hf-daily-paper-newsletter-chinese\\daily_papers\\daily_papers_2024-03-21.json", "r") as file:
+with open("D:\\python project\\hf-daily-paper-newsletter-chinese\\daily_papers\\daily_papers_2024-03-25.json", "r") as file:
     data = json.load(file)
 
 # 提取论文信息并按指定顺序排列
@@ -10,7 +10,7 @@ papers_clean = [{"title": paper["paper"]["title"],
                 "summary": paper["paper"]["summary"]} for paper in data]
 
 # 生成新文件名
-original_filename = "D:\\python project\\hf-daily-paper-newsletter-chinese\\daily_papers_clean\\daily_papers_2024-03-21.json"
+original_filename = "D:\\python project\\hf-daily-paper-newsletter-chinese\\daily_papers_clean\\daily_papers_2024-03-25.json"
 filename_parts = original_filename.split(".")
 new_filename = f"{filename_parts[0]}_clean.{filename_parts[1]}"
 
