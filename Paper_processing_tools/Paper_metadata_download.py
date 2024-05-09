b8d51f3bde3a11b596170eb76ffa822fb84112fc
King guys <68528929+2404589803@users.noreply.github.com>
2024-05-09 13:57:16 +0800

    更新hugging face每日论文查询方法

--- Paper_processing_tools/Paper_metadata_download.py
+++ Paper_processing_tools/Paper_metadata_download.py
@@ -2,9 +2,6 @@ import requests
 from datetime import datetime, timedelta
 import os
 
-# 设置遍历的天数
-days_to_check = 7
-
 # 指定保存数据的文件夹路径
 save_folder = "D:\\python project\\hf-daily-paper-newsletter-chinese\\daily_papers"
 
@@ -15,29 +12,26 @@ if not os.path.exists(save_folder):
 # 获取当前日期
 current_date = datetime.now()
 
-# 遍历前几天的日期
-for i in range(days_to_check):
-    # 计算查询的日期
-    query_date = (current_date - timedelta(days=i+1)).strftime('%Y-%m-%d')
-    
-    # 构建API URL
-    url = f"https://huggingface.co/api/daily_papers?date={query_date}"
-    
-    # 发送GET请求
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        # 检查是否有数据
-        if response.json():  # 如果返回的不是空列表
-            # 构建文件路径
-            file_path = os.path.join(save_folder, f'daily_papers_{query_date}.json')
-            with open(file_path, 'wb') as f:
-                f.write(response.content)
-            print(f"在 {query_date} 找到数据，已下载完成，保存在 {file_path}")
-            break  # 找到数据后退出循环
-        else:
-            print(f"在 {query_date} 没有找到数据")
+# 计算查询的日期(前一天)
+query_date = (current_date - timedelta(days=1)).strftime('%Y-%m-%d')
+
+# 构建API URL
+url = f"https://huggingface.co/api/daily_papers?date={query_date}"
+
+# 发送GET请求
+response = requests.get(url)
+
+if response.status_code == 200:
+    # 检查是否有数据
+    if response.json():
+        # 如果返回的不是空列表
+        # 构建文件路径
+        file_path = os.path.join(save_folder, f'daily_papers_{query_date}.json')
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
+        print(f"在 {query_date} 找到数据，已下载完成，保存在 {file_path}")
     else:
-        print(f"请求失败，状态码：{response.status_code}")
-        print(response.json())  # 打印出详细的错误信息
-        break  # 如果请求失败，退出循环
+        print(f"在 {query_date} 没有找到数据")
+else:
+    print(f"请求失败，状态码：{response.status_code}")
+    print(response.json())  # 打印出详细的错误信息
